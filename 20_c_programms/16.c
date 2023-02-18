3ba0fbee1ca12d4dfe2a1bcd16ec4fef964bae8a
PabloRout <120179609+PabloRout@users.noreply.github.com>
2023-02-18 20:40:23 +0530

    Update 16.c

--- 20_c_programms/16.c
+++ 20_c_programms/16.c
@@ -1,3 +1,7 @@
+/*Write a program which takes the radius of a circle as input from the user, passes it to
+ another function that computes the area and the circumference of the circle
+ and displays. the value of area and circumference from the main() function.*/
+
 #include <stdio.h>
 void circle(float , float*, float *);
 int main() 
